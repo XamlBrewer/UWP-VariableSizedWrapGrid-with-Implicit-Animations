c36d6cceaefe88445a72d8e1369b4101e8cda925
Diederik Krols <diederik_krols@hotmail.com>
2017-02-18 21:02:37 +0100

    Add project files.

---
+++ XamlBrewer.Uwp.ImplicitAnimation/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Mvvm.Services;
+using XamlBrewer.Uwp.ImplicitAnimation;
+
+namespace Mvvm
+{
+    internal class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            // Build the menus
+            Menu.Add(new MenuItem() { Glyph = Icon.GetIcon("BirdIcon"), Text = "Bird", NavigationDestination = typeof(MainPage) });
+
+            SecondMenu.Add(new MenuItem() { Glyph = Icon.GetIcon("InfoIcon"), Text = "About", NavigationDestination = typeof(AboutPage) });
+        }
+    }
+}
---
+++ XamlBrewer.Uwp.ImplicitAnimation/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace XamlBrewer.Uwp.ImplicitAnimation
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Yep: that's all.
+            GridView.RegisterImplicitAnimations();
+        }
+    }
+}
